1b6698c86676313d80b7c530921126162d5a6700
unknown <alexsei.koval@gmail.com>
2019-07-13 15:04:51 +0300

    commit 2

---
+++ build/precache-manifest.7e2a1cbbb1681a715ef7fe453733be4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c472da16f01565200706933396ec34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62fea32d88d477f254d8",
+    "url": "/static/css/main.170f56e8.chunk.css"
+  },
+  {
+    "revision": "f599750387396b1d2ba2",
+    "url": "/static/js/2.fc25e175.chunk.js"
+  },
+  {
+    "revision": "62fea32d88d477f254d8",
+    "url": "/static/js/main.eaf52922.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
